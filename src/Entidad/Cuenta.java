1d2b14c00e7ffdf6719f5c141118fa0a35111074
flan1412x <1214kurobakid@gmail.com>
2023-05-31 10:50:23 -0500

    Inicio de Proyecto

---
+++ src/Entidad/Cuenta.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidad;
+
+/**
+ *
+ * @author 1214k
+ */
+public class Cuenta {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public TipoCuenta getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(TipoCuenta tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the cedula
+     */
+    public String getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    private int id;
+    private double saldo;
+    private TipoCuenta tipo;
+    private String cedula; 
+    
+    
+}
