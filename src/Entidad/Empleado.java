3e6e6d97c956229028b9279a34f80029dd5eb6bd
Munir1001 <munircastromunoz@gmail.com>
2023-05-31 12:09:15 -0500

    Añadio Edad

--- src/Entidad/Empleado.java
+++ src/Entidad/Empleado.java
@@ -52,6 +52,14 @@ public class Empleado {
         this.apellido = apellido;
     }
     private String cedula, nombre, apellido; 
+    private int edad;
+
+    public Empleado(String cedula, String nombre, String apellido, int edad) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
     
    
 }
